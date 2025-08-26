e4dcaf22594d22ea3c4cd0f70d7c097c6ad1e693
Mikaela Duran <mikaeladuran93@gmail.com>
2025-08-26 14:20:47 +0200

    Initial commit

--- src/main/java/org/example/backend2/Backend2Application.java
+++ src/main/java/org/example/backend2/Backend2Application.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Backend2Application {
 
+
     public static void main(String[] args) {
         SpringApplication.run(Backend2Application.class, args);
     }
