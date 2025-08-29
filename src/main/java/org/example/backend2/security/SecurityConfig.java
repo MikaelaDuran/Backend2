49a620c8c9624ac64e7e22ee64b903a4db84ec06
Hani Kristiansson <hani.kristiansson@gmail.com>
2025-08-29 11:41:22 +0200

    work on SecurityConfig

--- src/main/java/org/example/backend2/security/SecurityConfig.java
+++ src/main/java/org/example/backend2/security/SecurityConfig.java
@@ -1,11 +1,13 @@
 package org.example.backend2.security;
 
 import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
-@Configurable
+@Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
+//Security Filter Chain
 
 }
