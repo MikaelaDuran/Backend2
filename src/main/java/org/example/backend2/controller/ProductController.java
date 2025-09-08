982e36aca774571d45a5e32d0a2c2ee8447aa57d
Kieran Borg <kierans.borg@gmail.com>
2025-09-08 14:43:05 +0200

    product controller

--- src/main/java/org/example/backend2/controller/ProductController.java
+++ src/main/java/org/example/backend2/controller/ProductController.java
@@ -1,10 +1,43 @@
 package org.example.backend2.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.example.backend2.models.Product;
+import org.example.backend2.service.ProductService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
 public class ProductController {
+
+    private final ProductService productService;
+
+    @GetMapping("/products/{category}")
+    public String getProductsByCategory(@PathVariable String category, Model model) {
+        List<Product> products = productService.getProductsByCategory(category);
+        model.addAttribute(products);
+        return "";
+    }
+    
+    @GetMapping("/products/{id}")
+    public String getProductById(@PathVariable long id, Model model)
+    {
+        Product product = productService.getProductById(id);
+        model.addAttribute(product);
+        return "";
+    }
+
+    @GetMapping("/products/all")
+    public String getAllProducts( Model model)
+    {
+        List<Product> products = productService.getAllProducts();
+        model.addAttribute(products);
+        return "";
+    }
+    
     
 }
--- src/main/java/org/example/backend2/service/ProductService.java
+++ src/main/java/org/example/backend2/service/ProductService.java
@@ -1,9 +1,29 @@
 package org.example.backend2.service;
 
+import lombok.RequiredArgsConstructor;
+import org.example.backend2.exceptions.ProductNotFoundException;
+import org.example.backend2.models.Product;
+import org.example.backend2.repository.ProductRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
+@RequiredArgsConstructor
 public class ProductService {
 
+    private final ProductRepository productRepository;
 
+    public List<Product> getProductsByCategory(String category) {
+        return productRepository.findAllByCategory(category);
+    }
+
+    public Product getProductById(long id) {
+        return productRepository.findById(id)
+                .orElseThrow(() -> new ProductNotFoundException(String.format("Product not found with id %d", id)));
+    }
+    
+    public List<Product> getAllProducts() {
+        return productRepository.findAll();
+    }
 }
