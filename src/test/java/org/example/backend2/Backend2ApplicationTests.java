3ae462cd01c2f5cc669770a1e865a34963fc2a8d
Hani Kristiansson <hani.kristiansson@gmail.com>
2025-10-02 12:07:57 +0200

    fixed test application properties

--- src/test/java/org/example/backend2/Backend2ApplicationTests.java
+++ src/test/java/org/example/backend2/Backend2ApplicationTests.java
@@ -2,8 +2,10 @@ package org.example.backend2;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class Backend2ApplicationTests {
 
     @Test
