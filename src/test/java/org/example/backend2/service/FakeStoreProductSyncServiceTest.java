96836625aa47b7a28e0507493bdf23f3f1083f7f
Hani Kristiansson <hani.kristiansson@gmail.com>
2025-10-02 13:16:51 +0200

    Branch is made to do test

---
+++ src/test/java/org/example/backend2/service/FakeStoreProductSyncServiceTest.java
@@ -0,0 +1,20 @@
+package org.example.backend2.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FakeStoreProductSyncServiceTest {
+
+    @Test
+    void getProductsFromApi() {
+    }
+
+    @Test
+    void syncProducts() {
+    }
+
+    @Test
+    void getProducts() {
+    }
+}
