1384a89f911089fe5b302c4b035242cac713db4d
Mikaela Duran <mikaeladuran93@gmail.com>
2025-10-07 19:46:39 +0200

    UserControllerTest test getAllUSers and postLogin

--- src/test/java/org/example/backend2/unit/controller/UserControllerTest.java
+++ src/test/java/org/example/backend2/unit/controller/UserControllerTest.java
@@ -13,7 +13,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
